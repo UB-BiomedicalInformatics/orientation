6239760ddf7ba51d060edaed22e5f3bc89521e21
jamie <jamie@buffalo.edu>
2018-07-09 10:35:57 -0400

    added R & RStudio setup template

---
+++ R/setup.R
@@ -0,0 +1,24 @@
+###########################################
+##      
+##      Set Up R and RStudio
+##      Workshop Date: 2018-7-11
+##      Prepared by Jamie Bono (jamie@buffalo.edu)
+##
+###########################################
+
+## Install Packages ====
+
+
+
+## Configure RStudio IDE ====
+
+
+
+## Creating Projects ====
+
+
+
+## Cloning Repositories ====
+
+
+
